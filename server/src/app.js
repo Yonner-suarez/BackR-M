62840850727e5bb12f749d5681c4434d8a8b9526
Yonner-suarez <yonnerhazziel@gmail.com>
2023-05-09 16:15:25 -0500

    prueba

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,8 @@ const miRouter = require("./routes/routes");
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://rick-and-morty-integration.vercel.app"
+    "http://localhost:3000"
+    //"https://rick-and-morty-integration.vercel.app"
   );
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
