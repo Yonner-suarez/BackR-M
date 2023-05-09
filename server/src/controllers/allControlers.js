9d9b4263b1440774ebab608c930ef09050dd0190
Yonner-suarez <yonnerhazziel@gmail.com>
2023-05-09 15:27:11 -0500

    backend

---
+++ server/src/controllers/allControlers.js
@@ -0,0 +1,13 @@
+const GetCharById = require("./GetCharById");
+const postUser = require("./postUser");
+
+const postFav = require("./postFav");
+const deleteFav = require("./deleteFav");
+
+module.exports = {
+  GetCharById,
+  postUser,
+  deleteFav,
+
+  postFav,
+};
