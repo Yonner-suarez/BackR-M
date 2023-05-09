7c0b8bb502edf7ae1ef00c7cb06afb658e727e7c
Yonner-suarez <yonnerhazziel@gmail.com>
2023-05-09 17:18:57 -0500

    cors

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const miRouter = require("./routes/routes");
+const cors = require("cors");
 
 app.use((req, res, next) => {
   res.header(
@@ -17,7 +18,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(morgan("dev"));
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/rickandmorty", miRouter);
--- server/src/controllers/logIn.js
+++ server/src/controllers/logIn.js
@@ -13,7 +13,6 @@ const login = async (req, res) => {
     if (!user) {
       return res.status(404).json({ error: "Usuario no encontrado" });
     }
-
     res.status(200).json({ user, access: true });
   } catch (error) {
     res.status(500).json({ error: error.message });
